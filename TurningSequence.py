6f5e734632ee2b22c78f3684cf8d749672122b64
IgnacioRubio3 <nachorubiobravo@gmail.com>
2026-09-09 20:00:54 +0000

    modified:   TurningSequence.py

--- TurningSequence.py
+++ TurningSequence.py
@@ -87,7 +87,7 @@ def main() -> None:
         
         # Step 1
         _drive(0.45, 0.45)
-        time.sleep(2.70)
+        time.sleep(2.26)
         _drive(0.0, 0.0)
 
         # Step 2
@@ -101,14 +101,14 @@ def main() -> None:
         s.wait_for_start()
         s.run_countdown()
         _drive(0.45, 0.0)
-        time.sleep(1.88)
+        time.sleep(1.7)
         _drive(0.0, 0.0)
 
         # Step 4
         s.wait_for_start()
         s.run_countdown()
         _drive(0.36, 0.62)
-        time.sleep(3.71)
+        time.sleep(3.20)
         _drive(0.0, 0.0)
 
         log.info("All 4 steps completed successfully!")
